78c148737803d8251ceea4827a050c291e40ace7
Julio Lima <julio_flima@hotmail.com>
2021-02-13 09:29:05 -0300

    Remove conflicts and readd Hello to the home page.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,13 @@
 import React from "react";
 import useTranslation from "../hooks/useTranslation";
-import { Translations } from "../translations/types";
 import LocaleLink from "./LocaleLink";
 
 export const Home = () => {
   const { t } = useTranslation();
-  const common = t("common") as Translations;
 
   return (
     <>
-      <h1>{common.navAbout}</h1>
+      <h1>{t("hello")}</h1>
       <p>
         <LocaleLink href="/about">
           <a>About</a>
