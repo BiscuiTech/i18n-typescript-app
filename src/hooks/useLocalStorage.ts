abda40eb03bd6b2f15da72c229796d57a21e5ea0
BiscuiTech <jhuet@biscui.tech>
2021-02-12 21:38:58 -0500

    Removed type any

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useLocalStorage(key: string, initialValue: any = "") {
+export function useLocalStorage(key: string, initialValue: unknown = "") {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
   const [storedValue, setStoredValue] = useState(() => {
